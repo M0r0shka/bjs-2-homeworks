172779f531ef1551cebbf7d70a67c424596b1ccf
Margarita Kosareva <kosareva-r@ya.ru>
2022-04-26 20:31:42 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length !== arr2.length) {
+    result = false;
+  } else {
+    result = arr1.every(compareEach);
+    function compareEach(item, idx) {
+      return item === arr2[idx];
+    }
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => (item > 0))
+                     .filter((item) => (item % 3 === 0))
+                     .map((item) => (item * 10));
   return resultArr; // array
 }

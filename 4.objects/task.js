ea12e1b725e10caf197a85878bb61125d216942a
Margarita Kosareva <kosareva-r@ya.ru>
2022-05-04 18:57:08 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name,
+  this.gender = gender,
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = Object.assign(marks);
+  } else {
+    for (let mark of marks) {
+      this.marks.push(mark);
+    }
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks !== undefined) {
+    const sum = this.marks.reduce((acc, item, idx, arr) => {
+      {
+        acc = acc + item;
+      }
+      return acc;
+    }, 0);
+    return (sum / this.marks.length);
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
